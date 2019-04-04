a507ca3410914608563998d8b896bd974fb6486a
Matthew Patterson <mdpatterson924@gmail.com>
2019-04-04 12:59:45 -0400

    Add project files.

---
+++ Wikipedia-Reddit.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wikipedia_Reddit.WebMVC.MvcApplication" Language="C#" %>
---
+++ Wikipedia-Reddit.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wikipedia_Reddit.WebMVC.Startup))]
+namespace Wikipedia_Reddit.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
